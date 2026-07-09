34e52000950175f634de7af757d3f22ffeebaccf
Divyanshu Khoiya <divyanshu98712@gmail.com>
2026-07-09 11:45:42 +0530

    Enemy Hit Point & Scoreboard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,31 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyVFX;
+    [SerializeField] int HitPoints = 3;
+    [SerializeField] int ScoreValue = 10;
+    Scoreboard scoreboard;
+
+    void Start()
+    {
+        scoreboard =  FindFirstObjectByType<Scoreboard>();
+    }
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyVFX, transform.position, destroyVFX.transform.rotation);
-        Destroy(gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        HitPoints--;
+
+        if (HitPoints <= 0)
+        {
+            scoreboard.ModifyScore(ScoreValue);
+            Instantiate(destroyVFX, transform.position, quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
